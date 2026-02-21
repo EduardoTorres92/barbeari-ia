9727e73033f7483577c6cd6527800a8ceb79ccd8
Eduardo Torres <eduardotorres.nexus@gmail.com>
2026-02-21 20:08:36 -0300

    feat: add Header componnent and Banner

---
+++ app/_components/ui/header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+import { Card, CardContent } from "@/app/_components/ui/card"
+import { Button } from "@/app/_components/ui/button"
+import { MenuIcon } from "lucide-react"
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="flex flex-row items-center justify-between p-5">
+        <Image src="/logo.png" alt="logo" width={120} height={120} />
+        <Button size="icon" variant="outline">
+          <MenuIcon className="h-4 w-4" />
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,36 @@
-"use client"
-
+import Header from "@/app/_components/ui/header"
+import { Input } from "@/app/_components/ui/input"
+import { SearchIcon } from "lucide-react"
 import { Button } from "@/app/_components/ui/button"
-export default function Home() {
-  return <Button> clica aqui</Button>
+import Image from "next/image"
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="mt-10 text-center text-xl font-bold">Olá, Eduardo!</h2>
+        <p className="mt-2 text-center text-sm">
+          Segunda Feira, 21 de Fevereiro
+        </p>
+
+        <div className="item-center mt-6 flex gap-2">
+          <Input placeholder="Pesquisar" className="w-full max-w-md" />
+          <Button>
+            <SearchIcon className="h-4 w-4" />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="agende nos melhores com sfw barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default Home
