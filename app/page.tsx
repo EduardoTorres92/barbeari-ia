30906a245403414bba2cf32faa6942790f2c1cd2
Eduardo Torres <eduardotorres.nexus@gmail.com>
2026-02-21 18:43:47 -0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-"use client";
+"use client"
 
-import { Button } from "@/app/_components/ui/button";
+import { Button } from "@/app/_components/ui/button"
 export default function Home() {
-  return <Button>clica aqui</Button>;
+  return <Button> clica aqui</Button>
 }
